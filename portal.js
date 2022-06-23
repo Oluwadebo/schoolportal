15c257b6d9fa8fad333093611a571004330f48e2
debo <ogunweoluwadebo@gmail.com>
2022-06-23 10:45:16 +0100

    js for sign up interface is completed

--- portal.js
+++ portal.js
@@ -1,30 +1,30 @@
-// allStudent = [];
-// function signup(){
-//     if (localStorage.myStudent) {
-//         allStudent = JSON.parse(localStorage.myStudent)
-//     }else{}
-//     let fName = firstname.value;
-//     let lName = lastname.value;
-//     let userEmail = email.value;
-//     let userAge = age.value;
-//     let userPassword = password.value;
-//     firstname.value = ""
-//     lastname.value = ""
-//     age.value = ""
-//     email.value = ""
-//     password.value = ""
-//     informatio = {
-//         firstname:fName,
-//         lastname: lName,
-//         email:userEmail,
-//         age:userAge,
-//         password:userPassword,
-//     }
-//     allStudent.push(informatio)
-//     localStorage.myStudent = JSON.stringify(allStudent)
-//     console.log(allStudent)
-//     location.replace('signin.html')
-// }
+allStudent = [];
+function signup(){
+    if (localStorage.myStudent) {
+        allStudent = JSON.parse(localStorage.myStudent)
+    }else{}
+    let fName = firstname.value;
+    let lName = lastname.value;
+    let userEmail = email.value;
+    let userAge = age.value;
+    let userPassword = password.value;
+    firstname.value = ""
+    lastname.value = ""
+    age.value = ""
+    email.value = ""
+    password.value = ""
+    informatio = {
+        firstname:fName,
+        lastname: lName,
+        email:userEmail,
+        age:userAge,
+        password:userPassword,
+    }
+    allStudent.push(informatio)
+    localStorage.myStudent = JSON.stringify(allStudent)
+    console.log(allStudent)
+    location.replace('signin.html')
+}
 // function siup() {
 //     localStorage.replace('signin.html')
 // }
