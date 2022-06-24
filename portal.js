0c3eae73ed2c856ec9a6e28e37c853fb24f12a9b
debo <ogunweoluwadebo@gmail.com>
2022-06-24 20:26:29 +0100

    delete button added to the dashboard page

--- portal.js
+++ portal.js
@@ -23,10 +23,10 @@ function signup(){
     allStudent.push(informatio)
     localStorage.myStudent = JSON.stringify(allStudent)
     console.log(allStudent)
-    location.replace('signin.html')
+    location.assign('signin.html')
 }
 function siup() {
-    localStorage.replace('signin.html')
+    localStorage.assign('signin.html')
 }
 function signIN() {
     let userEmail = email.value;
@@ -34,7 +34,7 @@ function signIN() {
     let students= JSON.parse(localStorage.getItem("myStudent"))
     for (let i = 0; i < students.length; i++) {
         if (userEmail == students[i].email && userPassword == students[i].password) {
-            location.replace('dashboard.html')
+            location.assign('dashboard.html')
         }else{
             if (userEmail != students[i].email) {
                 rong.innerHTML = `Incorrect email address`
@@ -48,13 +48,25 @@ function signIN() {
 
 }
 function sign() {
-    let allStudent= JSON.parse(localStorage.getItem("myStudent"))
+    let allStudent = JSON.parse(localStorage.getItem("myStudent"))
     let t = ""
     for (let i = 0; i < allStudent.length; i++) {
         t += `<tr>`
-        t += `<td>${i+1}</td><td>${allStudent[i].firstname}</td><td>${allStudent[i].lastname}</td><td>${allStudent[i].age}</td>`
+        t += `<td>${i+1}</td><td>${allStudent[i].firstname}</td><td>${allStudent[i].lastname}</td><td>${allStudent[i].age}</td><td><button onclick="dele()" type="submit" class="btn btn-danger">delet</button></td>`
         t += `</tr>` 
         console.log(allStudent[i])
     }
     display.innerHTML = t;
+}
+function dele(){
+    let answer = prompt("Enter your S/N");
+    let fians = answer - 1;
+    let allStudent = JSON.parse(localStorage.getItem("myStudent"))
+    for (let i = fians; i < allStudent.length;) {
+        if (confirm(`Are you sure, you want to delete S/N ${answer} details`) == true) {
+            allStudent.splice(allStudent, -1);
+            console.log(allStudent);
+        }
+    }
+    
 }
