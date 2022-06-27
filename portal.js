2b7c5e645de3315a9a1fc9f75366555962f165d6
debo <ogunweoluwadebo@gmail.com>
2022-06-27 13:13:10 +0100

    edit button is completed

--- portal.js
+++ portal.js
@@ -5,7 +5,7 @@ allStudent = [];
 function signup(){
     if (localStorage.myStudent) {
         allStudent = JSON.parse(localStorage.myStudent)
-    }else{}
+    }
     let fName = firstname.value;
     let lName = lastname.value;
     let userAge = age.value;
@@ -25,7 +25,7 @@ function signup(){
     }
     allStudent.push(informatio)
     localStorage.myStudent = JSON.stringify(allStudent)
-    console.log(allStudent)
+    // console.log(allStudent)
     location.assign('signin.html')
 }
 function siup() {
@@ -34,33 +34,28 @@ function siup() {
 function signIN() {
     let userEmail = email.value;
     let userPassword = password.value;
-    let students= JSON.parse(localStorage.getItem("myStudent"))
-    for (let i = 0; i < students.length; i++) {
-        if (userEmail == students[i].email && userPassword == students[i].password) {
+    allStudent= JSON.parse(localStorage.getItem("myStudent"))
+    for (let i = 0; i < allStudent.length; i++) {
+        if (userEmail != allStudent[i].email) {
+            rong.innerHTML = `Incorrect email address`
+            rong.style.color = "red"
+        }else if(userPassword != allStudent[i].password){
+            rng.innerHTML = `Incorrect password`
+            rng.style.color = "red"
+        }else if (userEmail == allStudent[i].email && userPassword == allStudent[i].password) {
+            rng.innerHTML = ""
             location.assign('dashboard.html')
-        }else{
-            if (userEmail != students[i].email) {
-                rong.innerHTML = `Incorrect email address`
-                rong.style.color = "red"
-            }else if(userPassword != students[i].password){
-                rng.innerHTML = `Incorrect password`
-                rng.style.color = "red"
-            }
+            break
         }
     }
 
 }
 function sign() {
-    let allStudent = JSON.parse(localStorage.getItem("myStudent"))
+    allStudent = JSON.parse(localStorage.getItem("myStudent"))
     let t = ""
     for (let i = 0; i < allStudent.length; i++) {
         t += `<tr>`
-        t += `<td>${i+1}</td>
-                <td>${allStudent[i].firstname}</td>
-                <td>${allStudent[i].lastname}</td>
-                <td>${allStudent[i].age}</td>
-                <td><button onclick="dele(${i})" type="submit" class="btn btn-danger">delet</button></td>
-                <td><button onclick="edit(${i})" type="submit" class="btn btn-primary">edit</button></td>`
+        t += "<td>"+[i+1]+"</td><td>"+allStudent[i].firstname+"</td><td>"+allStudent[i].lastname+"</td><td>"+allStudent[i].age+"</td><td><button class='btn btn-danger' onclick='dele("+i+")'>delet</button></td><td><button class='btn btn-primary' onclick='edit("+i+")'>edit</button></td>"
         t += `</tr>` 
         console.log(allStudent[i])
     }
@@ -69,8 +64,8 @@ function sign() {
 function dele(i){
     let answer = confirm("Are you sure, you want to delete")
     if (answer == true) {
-        let allStudent = JSON.parse(localStorage.getItem("myStudent"))
-        allStudent.splice((i,i))
+        allStudent = JSON.parse(localStorage.getItem("myStudent"))
+        allStudent.splice(i,1)
         console.log(allStudent);
         localStorage.myStudent = JSON.stringify(allStudent)
         location.reload()
@@ -78,25 +73,20 @@ function dele(i){
         alert("No changes was made")
     }
 }
+var edite;
 function edit(i){
-    let answer = confirm("Are you sure, you want to some changes")
-    if (answer == true) {
-        let allStudent = JSON.parse(localStorage.getItem("myStudent"))
-        let fName = prompt("Enter your FirstName")
-        let lName = prompt("Enter your LastName")
-        let userAge = prompt("Enter your age")
-        informatio = {
-            firstname:fName,
-            lastname: lName,
-            age:userAge,
-        }
-        allStudent.splice((i,i,"informatio"))
-        // fName = allStudent[i].firstname.value;
-        // lName = allStudent[i].lastname.value;
-        // userAge = allStudent[i].age.value;
-        // let onformation =  `${fName} ${lName} ${userAge}`
-        console.log(allStudent);
-    }else{
-        alert("No changes was made")
-    } 
+    frname.value = allStudent[i].firstname;
+    laname.value = allStudent[i].lastname;
+    age.value = allStudent[i].age;
+    edite = i;
+}
+function update(){
+    allStudent = JSON.parse(localStorage.getItem("myStudent"))
+    allStudent.splice(edite,1,{
+        firstname:frname.value,
+        lastname:laname.value,
+        age:age.value,
+    })
+    localStorage.myStudent = JSON.stringify(allStudent)
+    window.location.reload()
 }
